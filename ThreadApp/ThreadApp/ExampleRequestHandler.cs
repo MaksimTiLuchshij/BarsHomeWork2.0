bd798375a4619816857284b46dd7a7a9c2ed0585
Maksim <Min-max2000@mail.ru>
2022-04-12 16:30:34 +0300

    Add ThreadApp

---
+++ ThreadApp/ThreadApp/ExampleRequestHandler.cs
@@ -0,0 +1,22 @@
+﻿/// <summary>
+/// Предоставляет возможность обработать запрос.
+/// </summary>
+public interface IRequestHandler
+{
+    string HandleRequest(string message, string[] arguments);
+}
+
+
+public class ExampleRequestHandler : IRequestHandler
+{
+    public string HandleRequest(string message, string[] arguments)
+    {
+        // Притворяемся, что делаем что то.
+        Thread.Sleep(10_000);
+        if (message.Contains("упади"))
+        {
+            throw new Exception("Я упал, как сам просил");
+        }
+        return Guid.NewGuid().ToString("D");
+    }
+}
